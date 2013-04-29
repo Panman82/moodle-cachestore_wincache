cdab1af1310b022d68402aec7ba71f37597f1617
Ryan Panning <panman82@gmail.com>
2013-04-29 09:00:39 -0500

    Updated release name to reflect build number

--- wincache/version.php
+++ wincache/version.php
@@ -30,5 +30,5 @@ $plugin->requires  = 2012120300; // Moodle 2.4.0+
 $plugin->cron      = 0; // No cron jobs to run here
 $plugin->component = 'cachestore_wincache';
 $plugin->maturity  = MATURITY_RC;
-$plugin->release   = '1.0 (Build: 2013041800)';
+$plugin->release   = '1.0 (Build: 2013042900)';
 $plugin->dependencies = array(); // No dependencies
